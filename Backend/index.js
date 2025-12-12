81aaeede2d05682cf46776937757697e0b944387
Shailendra Pandey <shailendrakr2010@gmail.com>
2025-12-13 00:35:33 +0530

    Practice express JS

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import routerStudent from './Routers/studentRouter.js';
+import routerTeacher from './Routers/teacherRouter.js';
+
+const app = express();
+
+//  Build in Middleware
+app.use(express.json());
+
+// Router 
+app.use('/student',routerStudent);
+app.use('/teacher',routerTeacher);
+
+
+
+
+app.listen(5050, ()=>{
+    console.log("Server is Running...");
+})
